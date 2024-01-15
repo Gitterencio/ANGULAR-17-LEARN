e6ae9ea391be4e09c4f56383efba84b2dff1b345
Terencia Alberto Matamoros <tmatamoros@cognitiva.biz>
2024-01-15 08:56:58 -0600

    base rick&morty app

---
+++ r-m-ag-ionic-ag/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'r&m-ag-ionic-ag',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
